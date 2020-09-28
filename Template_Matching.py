6fd421bc8f19f081c7b7119321c104494c0c93fa
no7mac <44577325+no7macs@users.noreply.github.com>
2020-09-28 07:55:43 -0600

    they fly now? it works now?

--- Template_Matching.py
+++ Template_Matching.py
@@ -7,26 +7,23 @@ location = []
 location_list = []
 def match_image_multi(screencap,imgtemplate):
     searching = bool(True)
+    im = Image.open(screencap)
     while searching == True:
-        im = Image.open(screencap)
         img_rgb = np.array(im)
         img_gray = cv.cvtColor(img_rgb, cv.COLOR_BGR2GRAY)
         template = cv.imread(imgtemplate, 0)
-        template.shape[::-1]
+        x,y = template.shape[::-1]
         threshold = 0.8
         res = cv.matchTemplate(img_gray, template, cv.TM_CCOEFF_NORMED)
         min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
-        print(max_val)
         if max_val > threshold:
             location_list.append(max_loc)
             draw = ImageDraw.Draw(im)
-            draw.rectangle([(max_loc[0],max_loc[1]),(min_loc[0],min_loc[1])], fill='#ffffff', outline='#ffffff', width=1)
+            draw.rectangle([(max_loc[0],max_loc[1]),(max_loc[0]+x,max_loc[1]+y)], fill='#ffffff', outline='#ffffff', width=1)
             searching = True
-        else: 
-            im.show()
-            searching = False
-            return []
-    return max_loc
+            location_list.append([(max_loc[0],max_loc[1]),(x,y)])
+        else: searching = False
+    return location_list
 
 def match_image(screencap,imgtemplate):
     
