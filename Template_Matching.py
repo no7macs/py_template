1ebc1dde14d6e310a986372aba3ad83af0800d42
no7mac <44577325+no7macs@users.noreply.github.com>
2020-09-29 10:19:02 -0600

    broke it and fixed it again

--- Template_Matching.py
+++ Template_Matching.py
@@ -2,7 +2,7 @@ import cv2 as cv
 import numpy as np
 from matplotlib import pyplot as plt
 from PIL import Image, ImageGrab, ImageDraw
-import gc
+
 location = []
 location_list = []
 def match_image_multi(screencap,imgtemplate):
@@ -17,16 +17,14 @@ def match_image_multi(screencap,imgtemplate):
         res = cv.matchTemplate(img_gray, template, cv.TM_CCOEFF_NORMED)
         min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
         if max_val > threshold:
-            location_list.append(max_loc)
+            searching = True
             draw = ImageDraw.Draw(im)
             draw.rectangle([(max_loc[0],max_loc[1]),(max_loc[0]+x,max_loc[1]+y)], fill='#ffffff', outline='#ffffff', width=1)
-            searching = True
             location_list.append(max_loc)
         else: searching = False
     return location_list
 
 def match_image(screencap,imgtemplate):
-    
     im = Image.open(screencap)
     img_rgb = np.array(im)
     img_gray = cv.cvtColor(img_rgb, cv.COLOR_BGR2GRAY)
