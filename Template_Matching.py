8af7d2b4ec53850d717fd75b04a7cc8034c5803c
no7mac <44577325+no7macs@users.noreply.github.com>
2020-10-05 16:24:00 -0600

    A jank fix for my problem, will improve on and add to match_image_multi

--- Template_Matching.py
+++ Template_Matching.py
@@ -1,6 +1,5 @@
 import cv2 as cv
-import numpy as np
-from matplotlib import pyplot as plt
+from numpy import shape, array
 from PIL import Image, ImageGrab, ImageDraw
 
 location_list = []
@@ -10,37 +9,47 @@ def match_image_multi(imgtemplate, **kwargs):
 
     searching = bool(True)
     while searching == True:
-        img_rgb = np.array(im)
+        img_rgb = array(im)
         img_gray = cv.cvtColor(img_rgb, cv.COLOR_BGR2GRAY)
         template = cv.imread(imgtemplate, 0)
-        x,y = template.shape[::-1]
-        threshold = 0.8
-        res = cv.matchTemplate(img_gray, template, cv.TM_CCOEFF_NORMED)
-        min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
-        if max_val > threshold:
-            searching = True
-            draw = ImageDraw.Draw(im)
-            draw.rectangle([(max_loc[0],max_loc[1]),(max_loc[0]+x,max_loc[1]+y)], fill='#ffffff', outline='#ffffff', width=1)
-            location_list.append([(max_loc[0],max_loc[1]),(x,y)])
-        else: searching = False
-    return location_list
+        try:
+            x,y = template.shape[::-1]
+            threshold = 0.8
+            res = cv.matchTemplate(img_gray, template, cv.TM_CCOEFF_NORMED)
+            min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
+            if max_val > threshold:
+                searching = True
+                draw = ImageDraw.Draw(im)
+                draw.rectangle([(max_loc[0],max_loc[1]),(max_loc[0]+x,max_loc[1]+y)], fill='#ffffff', outline='#ffffff', width=1)
+                location_list.append([(max_loc[0],max_loc[1]),(x,y)])
+            else: searching = False
+        except: pass
+        return location_list
 
 def match_image(imgtemplate,**kwargs):
     if kwargs.get('screencap',None) == None: im = image_grab(size = kwargs.get('size',None))
     else: im = Image.open(kwargs.get('screencap'))
+    
+    if im == None: return([])
 
-    #im = Image.open(screencap)
-    img_rgb = np.array(im)
+    img_rgb = array(im)
     img_gray = cv.cvtColor(img_rgb, cv.COLOR_BGR2GRAY)
     template = cv.imread(imgtemplate, 0)
-    template.shape[::-1]
-    threshold = 0.8
-    res = cv.matchTemplate(img_gray, template, cv.TM_CCOEFF_NORMED)
-    min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
-    if max_val < threshold:
-        return []
-    return max_loc
+    try:
+        x,y = template.shape[::-1]
+        threshold = 0.8
+        res = cv.matchTemplate(img_gray, template, cv.TM_CCOEFF_NORMED)
+        min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
+        if max_val < threshold:
+            return []
+        else: return max_loc
+    except: pass
+    return []
 
 def image_grab(**kwargs):
-    screenshot = ImageGrab.grab(bbox = (kwargs.get('size',None)))
-    return(screenshot)
+    try:
+        screenshot = ImageGrab.grab(bbox = (kwargs.get('size',None)))
+        return(screenshot)
+    except: 
+        print('failed to grab screen')
+        return(None)
