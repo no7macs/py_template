bd88e06823d21cd2f37bbad4f43ad6f808fcc59d
no7mac <44577325+no7macs@users.noreply.github.com>
2020-10-20 21:38:08 -0600

    Removed old code comment, again untested

--- Template_Matching.py
+++ Template_Matching.py
@@ -11,22 +11,6 @@ def match_image_multi(imgtemplate, **kwargs):
 
     searching = bool(True)
     while searching == True:
-        #img_rgb = array(im)
-        #img_gray = cv.cvtColor(img_rgb, cv.COLOR_BGR2GRAY)
-        #template = cv.imread(imgtemplate, 0)
-        #try:
-        #    x,y = template.shape[::-1]
-        #    threshold = 0.8
-        #    res = cv.matchTemplate(img_gray, template, cv.TM_CCOEFF_NORMED)
-        #    min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
-        #    if max_val > threshold:
-        #        searching = True
-        #        draw = ImageDraw.Draw(im)
-        #        draw.rectangle([(max_loc[0],max_loc[1]),(max_loc[0]+x,max_loc[1]+y)], fill='#ffffff', outline='#ffffff', width=1)
-        #        location_list.append([(max_loc[0],max_loc[1]),(x,y)])
-        #    else: searching = False
-        #except: pass
-        #return location_list
 
         try:
             x,y = template.shape[::-1]
@@ -72,7 +56,7 @@ def match_image(imgtemplate,**kwargs):
         min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
         if max_val < threshold:
             return []
-        else: return max_loc
+        else: return (max_loc)
     except: 
         if Debug == True: print('Could not process template, is either caused by Template.shape failing or issue with either image') 
         else: pass
