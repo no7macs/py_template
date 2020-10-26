b49752c5fda8437729b5a5d5399f9501dec753d7
no7mac <44577325+no7macs@users.noreply.github.com>
2020-10-26 16:53:19 -0600

    fixed issua with multi finding

--- Template_Matching.py
+++ Template_Matching.py
@@ -10,12 +10,13 @@ def match_image_multi(imgtemplate, screencap = None, size = (1920,1080), threshh
 
     if im == None: return([])
 
-    img_rgb = array(im)
-    img_gray = cv.cvtColor(img_rgb, cv.COLOR_BGR2GRAY)
     template = cv.imread(imgtemplate, 0)
 
     searching = bool(True)
     while searching == True:
+
+        img_rgb = array(im)
+        img_gray = cv.cvtColor(img_rgb, cv.COLOR_BGR2GRAY)
 
         try:
             x,y = template.shape[::-1]
@@ -27,16 +28,19 @@ def match_image_multi(imgtemplate, screencap = None, size = (1920,1080), threshh
 
             res = cv.matchTemplate(img_gray, template, cv.TM_CCOEFF_NORMED)
             min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
+            print(max_val)
+            print(max_loc)
             if max_val > threshold:
                 searching = True
                 draw = ImageDraw.Draw(im)
                 draw.rectangle([(max_loc[0],max_loc[1]),(max_loc[0]+x,max_loc[1]+y)], fill='#ffffff', outline='#ffffff', width=1)
                 location_list.append([(max_loc[0],max_loc[1]),(x,y)])
+                im.show()
             else: searching = False
         except: 
             if Debug == True: print('Could not process template, is either caused by Template.shape failing or issue with either image') 
             else: pass
-        return location_list
+    return location_list
 
 def match_image(imgtemplate, screencap = None, size = (1920,1080), threshhold = 0.8, **kwargs):
     im = image_grab(size if not size == None else (1920,1080)) if screencap == None else screencap
