f23b387fc0f68ed3886451f529330c378f8ff79a
no7mac <44577325+no7macs@users.noreply.github.com>
2021-06-06 16:15:18 -0600

    setters and removed clutter

--- README.md
+++ README.md
@@ -5,3 +5,13 @@
   
 <h1>Description:</h1>
 An easy to use implementation of cv2 template matching in python for automation geared towards lowering the overhead used to speed up template matching on multiple images at a time.
+
+<h1>Goals:</h1>
+<BR>
+1. To create an easy way to use cv2 template matching that also uses oop to lower the overhead usedin batch image searches.
+<BR>
+2. To use modern python 3.8 features to have clean and modern code.
+<BR>
+3. To follow the minimalist ideology of being the bare bone basics needed to lower on bloat.
+<BR>
+4. To have pre compiled cpython binaries for fast and easy deployment.
--- Template_Matching.py
+++ Template_Matching.py
@@ -6,19 +6,22 @@ import time
 
 Debug = bool(True)
 
-class Template_Matching():
+class TemplateMatching():
 
-    def __init__(self, imgtemplate = None, screencap = None, size = None, threshhold = 0.8, multi_size = False, Debug = True, **kwargs):
+    def __init__(self, imgtemplate = None, screenCap = None, size = (1920, 1080), threshHold = 0.8, multi_size = False, Debug = True, **kwargs):
         #Should be able to declare all stuff ahead of time to lower on overhead
         self.imgtemplate = imgtemplate
-        self.screencap = screencap
+        self.screenCap = screenCap
         self.size = size
-        self.threshhold = threshhold
+        self.threshHold = threshHold
         self.multi_size = multi_size
         self.Debug = Debug
 
-    def templatesize(self, imgtemplate = None): return (cv.imread(self.imgtemplate if not self.imgtemplate is None else imgtemplate , 0)).shape[::-1]
+    #returns the size of the template
+    def getTamplateSize(self, imgtemplate = None): 
+        return (cv.imread(self.imgtemplate if not self.imgtemplate is None else imgtemplate , 0)).shape[::-1]
 
+    #grabs a screenshot
     def image_grab(self, size):
         try:
             return(ImageGrab.grab(bbox = (size if not size is None else(None))))
@@ -27,12 +30,13 @@ class Template_Matching():
             else: pass
             return(None)
 
-    def multi_size_match(self, screencap = None, imgtemplate = None):
+    '''
+    def multi_size_match(self, screenCap = None, imgtemplate = None):
         return
 
-    def match_image_multi(self, imgtemplate = None, screencap = None, size = None, threshhold = 0.8, multi_size = None, **kwargs):
-        threshhold = self.threshhold if not self.threshhold is None else threshhold if threshhold is None else 0.8
-        im = self.image_grab(self.size if not self.size is None else size) if not self.screencap is None else (self.screencap if not self.screencap is None else screencap)
+    def match_image_multi(self, imgtemplate = None, screenCap = None, size = None, threshHold = 0.8, multi_size = None, **kwargs):
+        threshHold = self.threshHold if not self.threshHold is None else threshHold if threshHold is None else 0.8
+        im = self.image_grab(self.size if not self.size is None else size) if not self.screenCap is None else (self.screenCap if not self.screenCap is None else screenCap)
         multi_size = self.multi_size if multi_size is None else multi_size
 
         location_list = []
@@ -47,7 +51,7 @@ class Template_Matching():
             
             max_val, max_loc, scale = self.match_image(imgtemplate, im, multi_size = multi_size)
 
-            if max_val > threshhold:
+            if max_val > threshHold:
                 searching = True
                 draw = ImageDraw.Draw(im)
                 draw.rectangle([(max_loc[0],max_loc[1]),(max_loc[0]+x,max_loc[1]+y)], fill='#ffffff', outline='#ffffff', width=1)
@@ -58,27 +62,46 @@ class Template_Matching():
                 time.sleep(2)
             else: searching = False
         return max_val_list,location_list
+    '''
 
-    def match_image(self, imgtemplate = None, screencap = None, size = None, threshhold = 0.8, multi_size = None, **kwargs):
-        threshhold = self.threshhold if not self.threshhold is None else threshhold if threshhold is None else 0.8
-        im = self.image_grab(self.size if not self.size is None else size) if not self.screencap is None else (self.screencap if not self.screencap is None else screencap)
-        multi_size = self.multi_size if multi_size is None else multi_size
+    def match_image(self, threshHold = 0.8, **kwargs):
+        threshHold = self.threshHold if not self.threshHold is None else 0.8 #grab treshold, if not given set to 0.8
+        im = self.image_grab(self.size) if self.screenCap is None else self.screenCap #grabs screensize if size is not given
 
         if im is None: return(0,(0,1))
 
         img_rgb = array(im)
         img_gray = cv.cvtColor(img_rgb, cv.COLOR_BGR2GRAY)
-        template = cv.imread(self.imgtemplate if not self.imgtemplate is None else imgtemplate, 0)
+        template = cv.imread(self.imgtemplate, 0)
         #template = cv.cvtColor(imgtemplate, cv.COLOR_BGR2GRAY)
         scale = int(1)
 
         try:
             min_val, max_val, min_loc, max_loc = cv.minMaxLoc(cv.matchTemplate(img_gray, template, cv.TM_CCOEFF_NORMED))
             
-            if max_val < threshhold:
+            if max_val < threshHold:
                 return 0,(0,1),0
             else: return max_val, max_loc, scale
         except IOError: 
             if self.Debug == True: print('Could not process template, is either caused by Template.shape failing or issue with either image') 
             else: pass
         return 0,(0,1),0
+
+    #------------------------------------------------------------------------------------#
+    #setters
+
+    def setImageTemplate(self, imgTemplate) -> None:
+        self.imgtemplate = imgTemplate
+        return()
+
+    def setscreenCap(self, screenCap) -> None:
+        self.screenCap = screenCap
+        return()
+
+    def setTheshHold(self, threshHold) -> None:
+        self.threshHold = threshHold
+        return()
+
+    def setSize(self, size) -> None:
+        self.size = size
+        return()
